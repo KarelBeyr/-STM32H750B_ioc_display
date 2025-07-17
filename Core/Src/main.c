66647bcd00614296e21428fc59d519fc0e93ac90
Karel Beyr <karel.beyr@cancom.com>
2025-07-17 18:31:09 +0200

    not-completely-ideal solution, but buildable now. If I leave MX_LTDC_Init in main, and instead I comment it out in stm32h750b_discovery_lcd.c, I get further errors, but maybe it would be better to solve those errors than to comment out generated code?

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stm32h750b_discovery_lcd.h"
+#include "stm32_lcd.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -84,7 +85,7 @@ static void MX_ADC3_Init(void);
 static void MX_FDCAN1_Init(void);
 static void MX_FDCAN2_Init(void);
 static void MX_FMC_Init(void);
-static void MX_LTDC_Init(void);
+//static void MX_LTDC_Init(void);
 static void MX_QUADSPI_Init(void);
 static void MX_RTC_Init(void);
 static void MX_SAI2_Init(void);
@@ -141,7 +142,7 @@ int main(void)
   MX_FDCAN1_Init();
   MX_FDCAN2_Init();
   MX_FMC_Init();
-  MX_LTDC_Init();
+  MX_LTDC_Init(&hltdc, 480, 272);
   MX_QUADSPI_Init();
   MX_RTC_Init();
   MX_SAI2_Init();
@@ -157,6 +158,11 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  BSP_LCD_Init(0, LCD_ORIENTATION_LANDSCAPE); // 0 = default layer
+  //BSP_LCD_Clear(0, LCD_COLOR_BLUE);
+  UTIL_LCD_Clear(UTIL_LCD_COLOR_BLUE);
+  //BSP_LCD_DisplayStringAt(0, 0, (uint8_t *)"Hello, STM32H750B-DK!", LEFT_MODE);
+  UTIL_LCD_DisplayStringAt(10, 10, (uint8_t *)"Hello world!", LEFT_MODE);
   while (1)
   {
     /* USER CODE END WHILE */
@@ -594,82 +600,82 @@ static void MX_FDCAN2_Init(void)
   * @param None
   * @retval None
   */
-static void MX_LTDC_Init(void)
-{
-
-  /* USER CODE BEGIN LTDC_Init 0 */
-
-  /* USER CODE END LTDC_Init 0 */
-
-  LTDC_LayerCfgTypeDef pLayerCfg = {0};
-  LTDC_LayerCfgTypeDef pLayerCfg1 = {0};
-
-  /* USER CODE BEGIN LTDC_Init 1 */
-
-  /* USER CODE END LTDC_Init 1 */
-  hltdc.Instance = LTDC;
-  hltdc.Init.HSPolarity = LTDC_HSPOLARITY_AL;
-  hltdc.Init.VSPolarity = LTDC_VSPOLARITY_AL;
-  hltdc.Init.DEPolarity = LTDC_DEPOLARITY_AL;
-  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
-  hltdc.Init.HorizontalSync = 40;
-  hltdc.Init.VerticalSync = 9;
-  hltdc.Init.AccumulatedHBP = 53;
-  hltdc.Init.AccumulatedVBP = 11;
-  hltdc.Init.AccumulatedActiveW = 533;
-  hltdc.Init.AccumulatedActiveH = 283;
-  hltdc.Init.TotalWidth = 565;
-  hltdc.Init.TotalHeigh = 285;
-  hltdc.Init.Backcolor.Blue = 0;
-  hltdc.Init.Backcolor.Green = 0;
-  hltdc.Init.Backcolor.Red = 0;
-  if (HAL_LTDC_Init(&hltdc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  pLayerCfg.WindowX0 = 0;
-  pLayerCfg.WindowX1 = 0;
-  pLayerCfg.WindowY0 = 0;
-  pLayerCfg.WindowY1 = 0;
-  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_ARGB8888;
-  pLayerCfg.Alpha = 0;
-  pLayerCfg.Alpha0 = 0;
-  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
-  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
-  pLayerCfg.FBStartAdress = 0;
-  pLayerCfg.ImageWidth = 0;
-  pLayerCfg.ImageHeight = 0;
-  pLayerCfg.Backcolor.Blue = 0;
-  pLayerCfg.Backcolor.Green = 0;
-  pLayerCfg.Backcolor.Red = 0;
-  if (HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  pLayerCfg1.WindowX0 = 0;
-  pLayerCfg1.WindowX1 = 0;
-  pLayerCfg1.WindowY0 = 0;
-  pLayerCfg1.WindowY1 = 0;
-  pLayerCfg1.PixelFormat = LTDC_PIXEL_FORMAT_ARGB8888;
-  pLayerCfg1.Alpha = 0;
-  pLayerCfg1.Alpha0 = 0;
-  pLayerCfg1.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
-  pLayerCfg1.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
-  pLayerCfg1.FBStartAdress = 0;
-  pLayerCfg1.ImageWidth = 0;
-  pLayerCfg1.ImageHeight = 0;
-  pLayerCfg1.Backcolor.Blue = 0;
-  pLayerCfg1.Backcolor.Green = 0;
-  pLayerCfg1.Backcolor.Red = 0;
-  if (HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg1, 1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN LTDC_Init 2 */
-
-  /* USER CODE END LTDC_Init 2 */
-
-}
+//static void MX_LTDC_Init(void)
+//{
+//
+//  /* USER CODE BEGIN LTDC_Init 0 */
+//
+//  /* USER CODE END LTDC_Init 0 */
+//
+//  LTDC_LayerCfgTypeDef pLayerCfg = {0};
+//  LTDC_LayerCfgTypeDef pLayerCfg1 = {0};
+//
+//  /* USER CODE BEGIN LTDC_Init 1 */
+//
+//  /* USER CODE END LTDC_Init 1 */
+//  hltdc.Instance = LTDC;
+//  hltdc.Init.HSPolarity = LTDC_HSPOLARITY_AL;
+//  hltdc.Init.VSPolarity = LTDC_VSPOLARITY_AL;
+//  hltdc.Init.DEPolarity = LTDC_DEPOLARITY_AL;
+//  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+//  hltdc.Init.HorizontalSync = 40;
+//  hltdc.Init.VerticalSync = 9;
+//  hltdc.Init.AccumulatedHBP = 53;
+//  hltdc.Init.AccumulatedVBP = 11;
+//  hltdc.Init.AccumulatedActiveW = 533;
+//  hltdc.Init.AccumulatedActiveH = 283;
+//  hltdc.Init.TotalWidth = 565;
+//  hltdc.Init.TotalHeigh = 285;
+//  hltdc.Init.Backcolor.Blue = 0;
+//  hltdc.Init.Backcolor.Green = 0;
+//  hltdc.Init.Backcolor.Red = 0;
+//  if (HAL_LTDC_Init(&hltdc) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  pLayerCfg.WindowX0 = 0;
+//  pLayerCfg.WindowX1 = 0;
+//  pLayerCfg.WindowY0 = 0;
+//  pLayerCfg.WindowY1 = 0;
+//  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_ARGB8888;
+//  pLayerCfg.Alpha = 0;
+//  pLayerCfg.Alpha0 = 0;
+//  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
+//  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
+//  pLayerCfg.FBStartAdress = 0;
+//  pLayerCfg.ImageWidth = 0;
+//  pLayerCfg.ImageHeight = 0;
+//  pLayerCfg.Backcolor.Blue = 0;
+//  pLayerCfg.Backcolor.Green = 0;
+//  pLayerCfg.Backcolor.Red = 0;
+//  if (HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg, 0) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  pLayerCfg1.WindowX0 = 0;
+//  pLayerCfg1.WindowX1 = 0;
+//  pLayerCfg1.WindowY0 = 0;
+//  pLayerCfg1.WindowY1 = 0;
+//  pLayerCfg1.PixelFormat = LTDC_PIXEL_FORMAT_ARGB8888;
+//  pLayerCfg1.Alpha = 0;
+//  pLayerCfg1.Alpha0 = 0;
+//  pLayerCfg1.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
+//  pLayerCfg1.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
+//  pLayerCfg1.FBStartAdress = 0;
+//  pLayerCfg1.ImageWidth = 0;
+//  pLayerCfg1.ImageHeight = 0;
+//  pLayerCfg1.Backcolor.Blue = 0;
+//  pLayerCfg1.Backcolor.Green = 0;
+//  pLayerCfg1.Backcolor.Red = 0;
+//  if (HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg1, 1) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /* USER CODE BEGIN LTDC_Init 2 */
+//
+//  /* USER CODE END LTDC_Init 2 */
+//
+//}
 
 /**
   * @brief QUADSPI Initialization Function
